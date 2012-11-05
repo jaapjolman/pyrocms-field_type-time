49d3387f8f3405620381ccd4a30bbac95ee2ab27
Antoine Benevaut <antoinebenevaut@Mac-mini-de-Antoine-Benevaut.local>
2012-11-06 00:47:20 +0100

    update copyright ^^

--- field.time.php
+++ field.time.php
@@ -1,7 +1,13 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
+ * PyroStreams Date/Time Field Type
  *
+ * @package		PyroCMS\Core\Modules\Streams Core\Field Types
+ * @author		Parse19
+ * @copyright	Copyright (c) 2011 - 2012, Parse19
+ * @license		http://parse19.com/pyrostreams/docs/license
+ * @link		http://parse19.com/pyrostreams
  */
 
 class Field_time
